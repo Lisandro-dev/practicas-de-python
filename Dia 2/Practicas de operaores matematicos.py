9146a5c551f60daeb26c26b659bbc4db7669fad5
Licha <lglafuente@gmail.com>
2024-07-22 19:05:40 -0300

    practica de operadores matematicos

---
+++ Dia 2/Practicas de operaores matematicos.py
@@ -0,0 +1,11 @@
+x=3
+y=5
+z=2
+# Practica de suma
+print(f"La suma de {x} e {y} es {x+y}")
+# Practica de división al piso
+print(f"La división al piso (redondeada) de {y} dividido {z} es {y//z}")
+# Practica de modulo
+print(f"El modulo de {y} dividido {z} es {y%z}")
+# Practica de potencia
+print(f"{z} elevado a la {5} da como resultado {z**5}")
