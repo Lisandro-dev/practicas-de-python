6730ca2423f1b94d8402887c65f799869a95dc82
Licha <lglafuente@gmail.com>
2024-07-22 18:51:52 -0300

    primer commit

---
+++ Dia 2/dia2.py
@@ -0,0 +1,6 @@
+x=10
+y=5
+color="rojo"
+print("Mis numeros son " + str(x)+ " y "+ str(y))
+print("Ahora son {} y {}".format(x,y))
+print(f"Tambien son {x} e {y}, y su suma es {x+y} y mi color es {color}")
