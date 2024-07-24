bc274c3b8950e1b14424bf1fb120fd8b94fe437e
Licha <lglafuente@gmail.com>
2024-07-24 19:35:24 -0300

    practica de operadores matematicos

---
+++ Dia 2/practica de redondeo.py
@@ -0,0 +1,9 @@
+print(round(90/7))
+numero=5
+operacion=numero/2
+print(round(operacion))
+valor=34,24325256
+dos_decimales=round(34.4325256,2)
+print(dos_decimales)
+print(type(dos_decimales))
+print(type(round(dos_decimales)))
