e31cf73f6869d66679f45c6bdbffe99338a66402
Licha <lglafuente@gmail.com>
2024-07-30 10:54:48 -0300

    slicing

---
+++ Dia 3/Slicing.py
@@ -0,0 +1,9 @@
+texto="Hola soy un texto"
+#Slicing o fragmentar, sirve para recortar palabras de una frase, tiene tres parametros, el rpimero
+#es desde donde empieza, el segundo donde termina (la posición anterior de la que establecemos)
+#el tercero son los valorres que podemos ir salteando (por ejemplo de 2 en 2 o de 3 en 3)
+#TODOS LOS VALORES VAN SEPARADOS POR DOS PUNTOS
+fragmento=texto[0:4]
+alternar=texto[0::2]
+print(fragmento)
+print(alternar)
