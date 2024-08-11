2eb705fb20aa1f80ba2a6e2d8f1d62ce1b360e0a
Licha <lglafuente@gmail.com>
2024-08-11 16:00:27 -0300

    booleanos

---
+++ Dia 3/booleanos.py
@@ -0,0 +1,7 @@
+#Un booleano devuelve un valor de true o false, verdadero o falso
+#Tambien podemos realizar comparaciones, o buscar un valor dentro de una lista
+var1= True
+var2=False
+print(type(var1))
+numero=5 < 2+3
+print(numero)
