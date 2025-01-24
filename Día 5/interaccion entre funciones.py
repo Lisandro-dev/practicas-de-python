80f0d014417faf0b7ac5a561465a925c92863d64
Licha <lglafuente@gmail.com>
2025-01-24 10:00:27 -0300

    con práctica 2

--- Día 5/interaccion entre funciones.py
+++ Día 5/interaccion entre funciones.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from random import randint
 
 #Lista inicial
 palitos = ['-','--','---','----']
@@ -23,4 +24,40 @@ def chequearIntento(lista, intento):
     print(f'te toco {lista[intento - 1]}')
     print(lista)
 
-chequearIntento(mezclar(palitos),probarSuerte())
+#chequearIntento(mezclar(palitos),probarSuerte())
+ #Práctica 1
+def lanzar_dados():
+    dado1 = randint(1, 6)
+    dado2 = randint(1, 6)
+    return dado1,dado2
+
+def evaluar_jugada(n1,n2):
+    suma=n1+n2
+    if suma<=6:
+        print(f"La suma de tus dados es {suma}. Lamentable")
+    elif suma > 6 and suma < 10:
+        print(f"La suma de tus dados es {suma}. Tienes buenas chances")
+    else:
+        print(f"La suma de tus dados es {suma}. Parece una jugada ganadora")
+dado1, dado2=lanzar_dados()
+evaluar_jugada(dado1,dado2)
+
+#Práctica 2
+lista_numero=[1,3,2,4,6,4,7,3,7,23]
+def reducir_lista(lista):
+    #Para eliminar los duplicados utilizo el método set
+    sin_duplicado=list(set(lista))
+    #ahora eliminamos el más alto
+    sin_duplicado.remove(max(sin_duplicado))
+    return sin_duplicado
+
+def promedio(lista):
+    #verifico que no este vacia con este if:
+    if lista:
+        return sum(lista) / len(lista)
+    return 0
+
+para_prom=reducir_lista(lista_numero)
+print(f"La lista reducida es {para_prom}")
+prom=promedio(para_prom)
+print(f"el promedio es {prom}")
