b488ba4dcf9477ef65fd69ba9ff49de17970549c
Licha <lglafuente@gmail.com>
2025-02-05 12:31:09 -0300

    ahorcado

--- Día 6/Escribir archivos.py
+++ Día 6/Escribir archivos.py
@@ -11,3 +11,36 @@ archivo.close()
 otro_archivo=open('Prueba1.txt', 'a')
 otro_archivo.write('Otra cosa')
 otro_archivo.close()
+
+ #Ejericio 1
+archivo=open('mi_archivo.txt', 'w')
+
+archivo.write('Nuevo texto')
+archivo.close()
+archivo=open('mi_archivo.txt')
+imprimir=archivo.read()
+
+print(imprimir)
+
+#Ejercicio 2
+archivo=open('mi_archivo.txt', 'a')
+
+archivo.write('Nuevo inicio de sesión')
+archivo.close()
+archivo=open('mi_archivo.txt')
+imprimir=archivo.read()
+
+print(imprimir)
+
+#Ejercicio 3
+
+archivo=open('registro.txt', 'a')
+registro_ultima_sesion = ["Federico", "20/12/2021", "08:17:32 hs", "Sin errores de carga"]
+
+for l in registro_ultima_sesion:
+    archivo.writelines(l+'\t')
+archivo.close()
+archivo=open('registro.txt')
+imprimir=archivo.read()
+
+print(imprimir)
