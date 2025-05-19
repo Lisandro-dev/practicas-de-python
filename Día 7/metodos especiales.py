3d0d50dce4de5172de6ad15e55b59cb479ad13a3
Licha <lglafuente@gmail.com>
2025-05-19 20:55:30 -0300

    metodos especiales

---
+++ Día 7/metodos especiales.py
@@ -0,0 +1,16 @@
+class CD:
+    def __init__(self, autor, titulo, canciones):
+        self.autor=autor
+        self.titulo=titulo
+        self.canciones=canciones
+    def __str__(self):
+        return f"Album: {self.titulo} de {self.autor}"
+
+    def __len__(self):
+        return self.canciones
+    def __del__(self):
+        print("Se ha eliminado el cd")
+mi_cd = CD('Led Zepellin', "IV", 12)
+del mi_cd
+
+
